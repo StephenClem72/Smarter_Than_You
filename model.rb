8251704b812a3b891418c89acea8229836e00b7d
Stephen Clem & Vincent Colavin <pair+stephenclem72.vcolavin@devbootcamp.com>
2014-11-28 15:56:10 -0800

    begin merge

--- model.rb
+++ model.rb
@@ -1,19 +1,21 @@
-# This is the interface that will query the questions.sqlite3 database in the local folder
+# This is the interface that will query the questions_db.csv "database" and return questions.
 require 'CSV'
 
 class Model
 
+  # returns hash of question and answer
+  # defaults to returning 1 question, else
+  # returns an array of length num_of_questions
   def self.get_question(num_of_questions = 1)
     list_of_questions = []
+
     CSV.foreach('questions_db.csv') do |question|
       list_of_questions << question
-
     end
-    Hash[list_of_questions.sample(num_of_questions)]
 
-    # returns hash of question and answer
-    # defaults to returning 1 question, else
-    # returns an array of length num_of_questions
+    # p list_of_questions
+
+    Hash[list_of_questions.sample(num_of_questions)]
   end
 end
 
@@ -21,5 +23,5 @@ end
 # ---
 # run time
 
-# p Model.get_question(4) # get 3 questions
+# p Model.get_question(4)
 
